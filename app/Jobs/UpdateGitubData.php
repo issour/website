3b70536836d8a3d8568f315050cdb8ba1d61a832
Brian Dillingham <bdillingham88@gmail.com>
2019-07-03 19:48:06 -0400

    Add github api info

---
+++ app/Jobs/UpdateGitubData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Workflow;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class UpdateGitubData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Workflow::all()->each->updateGithubData();
+    }
+}
--- resources/views/workflows/show.blade.php
+++ resources/views/workflows/show.blade.php
@@ -15,6 +15,18 @@
     <div class="container mx-auto pt-4 flex">
         <div class="w-4/5">
             <img src="/{{ $workflow->banner }}" alt="" class="w-full mb-4">
+            <div class="p-2">
+                <a href="https://github.com/{{ $workflow->repository }}" target="_blank" class="inline-block border border-gray-400 hover:border-gray-600 rounded-lg overflow-hidden">
+                    <span class="flex">
+                        <span class="bg-gray-200 py-2 px-4 font-bold align-middle">
+                            Stars
+                        </span>
+                        <span class="bg-white py-2 px-4">
+                            {{ $workflow->stars }}
+                        </span>
+                    </span>
+                </a>
+            </div>
             <div class="flex flex-wrap -mb-4 p-8 -mx-8">
                 <div>{{ $workflow->description }}</div>
             </div>
