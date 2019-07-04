8a114ab128479391e322a1b2de79809d0066b7d8
Brian Dillingham <bdillingham88@gmail.com>
2019-07-04 10:12:17 -0400

    Add markdown to mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
 
+mix.styles('resources/css/markdown.css', 'public/css/markdown.css');
 mix.sass('resources/sass/app.scss', 'public/css')
   .options({
     processCssUrls: false,
