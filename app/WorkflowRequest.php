936c808cee64f282aa634a3e1d1fbca8861946a5
Brian Dillingham <bdillingham88@gmail.com>
2019-07-06 20:58:28 -0400

    Add workflow request model

---
+++ app/WorkflowRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WorkflowRequest extends Model
+{
+    public $appends = ['status'];
+
+    public function getStatusAttribute()
+    {
+        if ($this->approved_at) {
+            return 'approved';
+        }
+
+        if ($this->rejected_at) {
+            return 'rejected';
+        }
+
+        return 'requested';
+    }
+}
