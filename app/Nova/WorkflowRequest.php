33b650740ce097f72d26d37e51b3af690dbfc460
Brian Dillingham <bdillingham88@gmail.com>
2019-07-06 20:58:41 -0400

    Add workflow request nova resource

---
+++ app/Nova/WorkflowRequest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use App\Nova\Actions\Reject;
+use App\Nova\Filters\Status;
+use Illuminate\Http\Request;
+use App\Nova\Actions\Approve;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Badge;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class WorkflowRequest extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\WorkflowRequest';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'title',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Title')->rules('required'),
+            Textarea::make('Description')->rules('required'),
+            Text::make('Url')->rules('required'),
+            Badge::make('Status')->types([
+                'requested' => 'bg-20',
+                'rejected' => 'bg-danger',
+                'approved' => 'bg-success',
+            ]),
+            DateTime::make('Approved At')->onlyOnDetail(),
+            DateTime::make('Rejected At')->onlyOnDetail(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [
+            new Status,
+        ];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [
+            new Approve,
+            new Reject,
+        ];
+    }
+}
