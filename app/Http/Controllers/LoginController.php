0881df711ae9cd6c0ff25ed18328f92203a8d798
Brian Dillingham <bdillingham88@gmail.com>
2019-07-06 21:51:41 -0400

    Add github login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    /**
+      * Redirect the user to the GitHub authentication page.
+      *
+      * @return \Illuminate\Http\Response
+      */
+    public function redirect()
+    {
+        return Socialite::driver('github')->stateless()->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function callback()
+    {
+        try {
+            $user = Socialite::driver('github')->stateless()->user();
+        } catch (\Exception $e) {
+            return redirect()->route('login.github');
+        }
+
+        $user = User::firstOrCreate(['github' => $user->nickname ], [
+            'name' => $user->name,
+            'email' => $user->email,
+            'github' => $user->nickname,
+            'password' => Str::random(),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('home');
+    }
+}
