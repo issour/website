c0359ecc8392074df7d6eb48e84a509cdf80cbc8
Brian Dillingham <bdillingham88@gmail.com>
2019-07-10 02:16:42 -0400

    Dedicate test for proposal approval

---
+++ tests/Feature/ProposalApprovalTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\App;
+use App\Proposal;
+use App\Workflow;
+use Tests\TestCase;
+use GuzzleHttp\Client;
+use App\Jobs\ApproveProposal;
+use App\Jobs\DeleteWorkflowCleanup;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProposalApprovalTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testApprovingProposals()
+    {
+        $proposal = factory(Proposal::class)->state('with-logo')->create([
+            'repository' => 'some-repo-name' . rand(500, 1000),
+            'stub' => 'stubs/outcome',
+            'app_id' => factory(App::class),
+        ]);
+
+        $this->mock(Client::class, function ($mock) {
+            $mock->shouldReceive('request');
+        });
+
+        dispatch(new ApproveProposal($proposal));
+
+        $this->assertEquals(1, Workflow::staging()->count());
+        $this->assertEquals('draft', Workflow::staging()->first()->status);
+    }
+}
