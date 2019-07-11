ae95f0dc5aa6e52f44d03ecec420b443b76720c2
Brian Dillingham <bdillingham88@gmail.com>
2019-07-11 15:20:38 -0400

    Change workflows to votes on user model

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function workflows()
+    public function votes()
     {
         return $this->belongsToMany(Workflow::class, 'votes');
     }
