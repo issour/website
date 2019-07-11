3b57a578b8337eaf682e678a55baf06c20d09f59
Brian Dillingham <bdillingham88@gmail.com>
2019-07-11 03:43:01 -0400

    Add notification assertions to launch test

--- tests/Feature/WorkflowLaunchTest.php
+++ tests/Feature/WorkflowLaunchTest.php
@@ -4,8 +4,11 @@ namespace Tests\Feature;
 
 use App\Workflow;
 use Tests\TestCase;
+use App\Subscription;
 use App\Jobs\LaunchWorkflow;
+use App\Notifications\WorkflowLaunch;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WorkflowLaunchTest extends TestCase
@@ -31,5 +34,22 @@ class WorkflowLaunchTest extends TestCase
             $this->assertTrue($workflow->isPublished());
             $this->assertFalse($workflow->inStaging());
         });
+    }
+
+    public function testLaunchNotifiesSubscribers()
+    {
+        Notification::fake();
+
+        $workflow = factory(Workflow::class)->state('staging')->create();
+
+        factory(Subscription::class, 10)->create(['workflow_id' => $workflow->id]);
+
+        dispatch(new LaunchWorkflow($workflow));
+
+
+        Notification::assertSentTo(
+            $workflow->subscribers,
+            WorkflowLaunch::class
+        );
     }
 }
