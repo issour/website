7145c75ef23f821f3d269a3cc9d56f5694baf117
Brian Dillingham <bdillingham88@gmail.com>
2019-07-12 12:19:57 -0400

    Add more proposal factory states

--- database/factories/ProposalFactory.php
+++ database/factories/ProposalFactory.php
@@ -2,6 +2,7 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
+use App\App;
 use App\Proposal;
 use Faker\Generator as Faker;
 
@@ -11,6 +12,18 @@ $factory->define(Proposal::class, function (Faker $faker) {
         'description' => $faker->paragraph,
         'url' => $faker->url,
     ];
+});
+
+$factory->state(Proposal::class, 'action-fields', function (Faker $faker) {
+    return [
+        'app_id' => factory(App::class),
+        'repository' => $faker->word,
+    ];
+});
+
+$factory->afterMakingState(Proposal::class, 'action-fields', function ($proposal, $faker) {
+    mkdir($proposal->path());
+    copy(base_path('tests/Assets/logo.png'), $proposal->path('logo.png'));
 });
 
 $factory->afterMakingState(Proposal::class, 'with-logo', function ($proposal, $faker) {
@@ -33,5 +46,17 @@ $factory->state(Proposal::class, 'approved', function (Faker $faker) {
 $factory->state(Proposal::class, 'rejected', function (Faker $faker) {
     return [
         'rejected_at' => now()->subDays(rand(1, 30)),
+    ];
+});
+
+$factory->state(Proposal::class, 'with-app', function (Faker $faker) {
+    return [
+        'app_id' => factory(App::class),
+    ];
+});
+
+$factory->state(Proposal::class, 'with-repo', function (Faker $faker) {
+    return [
+        'repository' => $faker->word,
     ];
 });
